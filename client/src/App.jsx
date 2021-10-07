0a9635e478214fa8dd651f1b82af64bf5b70c5f1
swarajdhore <dhoreswaraj@gmail.com>
2021-10-07 07:50:44 +0530

    Food Tag Component

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import HomeLayoutHOC from "./HOC/Home.HOC";
+import Temp from "./Components/temp";
+
+function App() {
+  return (
+    <div className="App">
+      <HomeLayoutHOC path="/:type" exact component={Temp} />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Layout/Home.layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import FoodTab from "../Components/FoodTabs";
+
+//Components
+import Navbar from "../Components/Navbar";
+
+function HomeLayout({ children }) {
+  return (
+    <div>
+      <Navbar />
+      <FoodTab />
+      <div className="container mx-auto px-4 lg:px-20">{children}</div>
+    </div>
+  );
+}
+
+export default HomeLayout;
