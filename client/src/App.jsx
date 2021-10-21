40293c9cb6ddc04ebb076f93acc9c37c13012316
swarajdhore <dhoreswaraj@gmail.com>
2021-10-21 10:48:06 +0530

    Created Map Component and Menu, Review, Photo Tabs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,10 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Redirect, Route } from "react-router";
 import Overview from "./pages/Overview";
+import OrderOnline from "./pages/OrderOnline";
+import Reviews from "./pages/Reviews";
+import Menu from "./pages/Menu";
+import Photos from "./pages/Photos";
 
 function App() {
   return (
@@ -28,18 +32,18 @@ function App() {
       <RestaurantLayoutHOC
         path="/restaurant/:id/order-online"
         exact
-        component={Temp}
+        component={OrderOnline}
       />
-      <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Temp} />
+      <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Menu} />
       <RestaurantLayoutHOC
         path="/restaurant/:id/reviews"
         exact
-        component={Temp}
+        component={Reviews}
       />
       <RestaurantLayoutHOC
         path="/restaurant/:id/photos"
         exact
-        component={Temp}
+        component={Photos}
       />
     </div>
   );
